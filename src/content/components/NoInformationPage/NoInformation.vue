9eda520536440359347c83174241809662f392e8
Nikolai Maidan <axroit.08@gmail.com>
2022-11-01 01:56:47 +0400

    add new component as if there are no data from api

---
+++ src/content/components/NoInformationPage/NoInformation.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="no-data">
+    Sorry there are no information yet
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.no-data {
+  font-size: 20px;
+  font-family: 'Gilroy', sans-serif;
+  color: #FFFFFF;
+  position: relative;
+  top: 155px;
+  width: 310px;
+  height: 45px;
+  left: 35px;
+  border: 1px solid #7276FF;
+  border-radius: 5px;
+  padding: 10px 10px;
+  display: flex;
+  align-items: center;
+}
+</style>
